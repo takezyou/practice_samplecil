dcb42595c9a3fa35a9eb260dfbdf5f811cec4ba8
Kaito Akita <kaitokun07@icloud.com>
2021-04-18 01:45:37 +0900

    add 163p

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "takezyou",
+    about = "Super awesome sample RPN calculator"
+)]
+
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        0
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, opts.verbose);
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let answer = calc.eval(&line);
+        println!("{}", answer);
+    }
 }
